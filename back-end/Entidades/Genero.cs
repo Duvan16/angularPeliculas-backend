115818f405e2e7bd0cd8fea0703b2b47edf15bf9
Duvan16 <duvan4448@gmail.com>
2021-09-26 17:16:06 -0500

    5-ValidacionesPersonalizadas

--- back-end/Entidades/Genero.cs
+++ back-end/Entidades/Genero.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using back_end.Validaciones;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -6,11 +7,13 @@ using System.Threading.Tasks;
 
 namespace back_end.Entidades
 {
-    public class Genero
+    public class Genero: IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength: 10)]
+
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         [Range(18, 120)]
         public int Edad { get; set; }
@@ -20,6 +23,18 @@ namespace back_end.Entidades
         [Url]
         public string URL { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
 
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayúscula...",
+                        new string[] { nameof(Nombre) });
+                }
+            }
+        }
     }
 }
