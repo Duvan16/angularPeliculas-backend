3bcf19f32a2be0636be0cd3bc79c2b608a8f8b3f
Duvan16 <duvan4448@gmail.com>
2021-09-26 15:35:46 -0500

    4-Validaciones

--- back-end/Controllers/GenerosController.cs
+++ back-end/Controllers/GenerosController.cs
@@ -1,6 +1,7 @@
 ﻿using back_end.Entidades;
 using back_end.Repositorios;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace back_end.Controllers
 {
     [Route("api/generos")] //Endpoint
     //[Route("api/[controller]")]
+    [ApiController]
     public class GenerosController : ControllerBase
     {
         private readonly IRepositorio repositorio;
@@ -27,8 +29,8 @@ namespace back_end.Controllers
 
         //[HttpGet("ejemplo")]
         //[HttpGet("{Id}")]
-        [HttpGet("{Id:int}/{nombre=Roberto}")] //api/generos/3/Felipe
-        public async Task<ActionResult<Genero>> Get(int Id, string nombre)
+        [HttpGet("{Id:int}")] //api/generos/3/Felipe
+        public async Task<ActionResult<Genero>> Get(int Id, [FromHeader] string nombre)
         {
             var genero = await repositorio.ObtenerPorId(Id);
             if (genero == null)
@@ -39,6 +41,12 @@ namespace back_end.Controllers
             return genero;
             //return Ok(genero); //IActionResult
             //return Ok("Felipe");
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Genero genero)
+        {
+            return NoContent();
         }
 
     }
