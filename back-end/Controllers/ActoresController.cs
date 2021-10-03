6fa0b3084cf5a5122a82d53051b8a03961e35000
Duvan16 <duvan4448@gmail.com>
2021-10-03 07:21:48 -0500

    41-Peliculas-Crear-SelectorMultiple

--- back-end/Controllers/ActoresController.cs
+++ back-end/Controllers/ActoresController.cs
@@ -50,6 +50,17 @@ namespace back_end.Controllers
             return mapper.Map<ActorDTO>(actor);
         }
 
+        [HttpPost("buscarPorNombre")]
+        public async Task<ActionResult<List<PeliculaActorDTO>>> BuscarPorNombre([FromBody] string nombre)
+        {
+            if (string.IsNullOrWhiteSpace(nombre)) { return new List<PeliculaActorDTO>(); }
+            return await context.Actores
+                .Where(x => x.Nombre.Contains(nombre))
+                .Select(x => new PeliculaActorDTO { Id = x.Id, Nombre = x.Nombre, Foto = x.Foto })
+                .Take(5)
+                .ToListAsync();
+        }
+
         [HttpPost]
         public async Task<ActionResult> Post([FromForm] ActorCreacionDTO actorCreacionDTO)
         {
