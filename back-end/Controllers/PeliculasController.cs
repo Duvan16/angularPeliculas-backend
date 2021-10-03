12e8a8fb188e7b3596e1e1f00532eaed5a4bfb84
Duvan16 <duvan4448@gmail.com>
2021-10-03 13:04:23 -0500

    46-Pelicula-Borrar

--- back-end/Controllers/PeliculasController.cs
+++ back-end/Controllers/PeliculasController.cs
@@ -202,5 +202,23 @@ namespace back_end.Controllers
             await context.SaveChangesAsync();
             return NoContent();
         }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var pelicula = await context.Peliculas.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(pelicula);
+            await context.SaveChangesAsync();
+
+            await almacenadorArchivos.BorrarArchivo(pelicula.Poster, contenedor);
+
+            return NoContent();
+        }
     }
 }
